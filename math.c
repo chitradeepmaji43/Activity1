c085b75d957960216f85fb0ed140eec42654b61d
chitradeepmaji43 <119390461+chitradeepmaji43@users.noreply.github.com>
2022-11-29 10:10:13 +0530

    Add files via upload

---
+++ math.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main();
+{
+int a=12, b=25;
+printf("output =%d", a&b);
+return0;
+}
+
+#2nd code
+
+#include<stdio.h>
+int main()
+{
+int a=12, b =25;
+printf("Output= %d", a/b);
+return0;
+}
+
